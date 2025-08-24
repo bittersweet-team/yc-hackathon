b79ad12f75789b959167c1ef5b4c567ae7a07054
SHGroup <kshgroup@kshgroup.kr>
2025-08-24 11:12:40 +0900

    fix: stop auto reload

--- server/main.py
+++ server/main.py
@@ -120,6 +120,6 @@ if __name__ == "__main__":
         "main:app",
         host="0.0.0.0",
         port=settings.port,
-        reload=True,
+        reload=False,
         log_level="info"
     )
